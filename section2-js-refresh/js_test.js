d191612b8c5bf7dc48a0ebcf1e2467666c25b842
Adnan Ahmad Haruna <ahmadadnan2235@gmail.com>
2023-04-08 02:06:02 +0100

    starting out react app

--- section2-js-refresh/js_test.js
+++ section2-js-refresh/js_test.js
@@ -1,6 +1,15 @@
+// function transformToObjects(numberArray) {
+//   return numberArray.map((number) => {
+//     return { val: number };
+//   });
+// }
+
+
 function transformToObjects(numberArray) {
-  return numberArray.map((number) => {
-    return { val: number };
+  // Todo: Add your logic
+  return numberArray.map((n) => {
+    return { val: n };
   });
+  // should return an array of objects
 }
 console.log(transformToObjects([1,2,3]))
